64121e50a568c627d86923f3792d09cfa8b5cf2a
Disty0 <semihgulec2005@gmail.com>
2023-04-24 00:22:00 +0300

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -14,7 +14,7 @@ bin/micromamba create -f environments/rocm.yml -r runtime -n koboldai-rocm -y
 bin/micromamba create -f environments/rocm.yml -r runtime -n koboldai-rocm -y
 exit
 fi
-if [[ $1 = "oneapi" ]]; then
+if [[ $1 = "oneapi" || $1 = "ONEAPI" || $1 = "OneAPI" || $1 = "oneAPI" ]]; then
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 bin/micromamba create -f environments/oneapi.yml -r runtime -n koboldai-oneapi -y
 # Weird micromamba bug causes it to fail the first time, running it twice just to be safe, the second time is much faster
