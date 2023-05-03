2c5cdfd254660bb18f573164f5ef6dcec870466b
Disty0 <semihgulec2005@gmail.com>
2023-05-03 23:36:55 +0300

    Use ipexrun

--- play-oneapi.sh
+++ play-oneapi.sh
@@ -5,4 +5,4 @@ if [ ! -f "runtime/envs/koboldai-oneapi/bin/python" ]; then
 fi
 source /opt/intel/oneapi/setvars.sh
 export IPEX=1
-bin/micromamba run -r runtime -n koboldai-oneapi python aiserver.py --use-ipex --use-channels-last $*
+bin/micromamba run -r runtime -n koboldai-oneapi ipexrun aiserver.py --use-ipex --use-channels-last $*
