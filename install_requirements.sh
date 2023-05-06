b734070dd7d25e5493f964fabb11a8756653fcbd
Disty0 <semihgulec2005@gmail.com>
2023-05-06 22:34:27 +0300

    Fix torch version error

--- install_requirements.sh
+++ install_requirements.sh
@@ -20,6 +20,8 @@ wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -
 bin/micromamba create -f environments/oneapi.yml -r runtime -n koboldai-oneapi -y
 # Weird micromamba bug causes it to fail the first time, running it twice just to be safe, the second time is much faster
 bin/micromamba create -f environments/oneapi.yml -r runtime -n koboldai-oneapi -y
+#Wants torch>=1.13.0 but can't find it even tough we have it.
+bin/micromamba run -r runtime -n koboldai-oneapi pip install --no-deps peft==0.3.0 accelerate==0.18.0
 exit
 fi
 echo Please specify either CUDA or ROCM or oneAPI
