54853f010eea4a14eb741373430976a5ebc9e882
Disty0 <semihgulec2005@gmail.com>
2023-05-09 02:16:44 +0300

    remove export IPEX=1

--- play-oneapi.sh
+++ play-oneapi.sh
@@ -4,5 +4,4 @@ if [ ! -f "runtime/envs/koboldai-oneapi/bin/python" ]; then
 ./install_requirements.sh oneapi
 fi
 source /opt/intel/oneapi/setvars.sh
-export IPEX=1
 bin/micromamba run -r runtime -n koboldai-oneapi ipexrun aiserver.py --use-channels-last $*
